467ff856cfe732900b012c9d56d4f3a1a27915fd
unknown <Nina@LT-17.vadis.local>
2015-05-22 13:44:49 -0700

    completed input to extent I know how, next steps include income formulas and household info

--- sandbox/web_root/js_functions_output.js
+++ sandbox/web_root/js_functions_output.js
@@ -1,10 +1,10 @@
 //radio_button_group_function=name of the funtion; radio_switches=what it effects
 
-
+//function     name(name of parameter 1, name of parameter 2) {code to be excecuted}
 function radio_button_group_function(radio_button_groupname)
 {
 
-var radio_button_group_var = document.getElementsByName(groupname);
+var radio_button_group_var = document.getElementsByName(radio_button_groupname);
 
 for(var i = 0; i < radio_button_group_var.length; i++) {
    if(radio_button_group_var[i].checked == true) {
@@ -15,6 +15,10 @@ return(selected_option);
 
 }
 
+// function check() {
+//     alert(document.getElementById("disability_status").checked);
+// localStorage.setItem("disability_status", document.getElementById("disability_status").checked);
+// }
 
 //alert(Working?)
 //alert("I am an alert box!");
